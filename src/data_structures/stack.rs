38fd282ac5f7d326d3e3e2af7b373001817128dc
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2023-11-02 17:19:46 +0300

    add stack structure

--- src/data_structures/stack.rs
+++ src/data_structures/stack.rs
@@ -1,24 +1,40 @@
-fn _new_stack(capacity: usize) -> Vec<i32> {
-    return Vec::with_capacity(capacity);
+struct StackNode<T: Copy> {
+    data: T,
+    prev: Option<Box<StackNode<T>>>,
+
 }
 
-fn _push(stack: &mut Vec<i32>, value: i32) {
-    stack.push(value);
+
+struct Stack<T: Copy> {
+    head: Option<Box<StackNode<T>>>,
+    length: usize,
 }
 
-fn _pop(stack: &mut Vec<i32>) -> Option<i32> {
-    return stack.pop();
-}
+impl<T: Copy> Stack<T> {
+    fn _push(&mut self, data: T) {
+        let node = StackNode {
+            data: data,
+            prev: self.head.take(),
+        };
+        self.head = Some(Box::new(node));
+        self.length += 1;
+    }
 
-pub fn _stack() {
-    let mut stack = _new_stack(3);
-    _push(&mut stack, 1);
-    _push(&mut stack, 2);
-    _push(&mut stack, 3);
-    _push(&mut stack, 4);
+    fn _pop(&mut self) -> Option<T> {
+        match self.head.take() {
+            Some(node) => {
+                self.head = node.prev;
+                self.length -= 1;
+                Some(node.data)
+            },
+            None => None,
+        }
+    }
 
-    println!("Stack: {:?}", stack);
-
-    let value = _pop(&mut stack).unwrap();
-    println!("Popped value: {:?}", value);
+    fn _peek(&self) -> Option<&T> {
+        match self.head.as_ref() {
+            Some(node) => Some(&node.data),
+            None => None,
+        }
+    }
 }
